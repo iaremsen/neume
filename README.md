0385f94d68d730667db785516207b8994e5b7a9f
Ian Remsen <ianremsen@tilde.team>
2023-05-17 03:07:24 -0400

    lazy

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # neume
+a programmatic medium for audio and music to facilitate composition, analysis, and experimentation
--- src/Music/Tonality/CommonPractice.fs
+++ src/Music/Tonality/CommonPractice.fs
@@ -8,29 +8,30 @@ open System
        one learns in an Introduction to Music Theory class, plus descendant
        ideas and techniques used in popular recorded music.
 ***)
+
+(***
+        SCALES
+***)
+
 // TO-DO integrate and make rigid beyond chromatic scale/12EDO
-type PCLetter = A | B | C | D | E | F | G // best way to do this?
-type Accidental = Flat | Natural | Sharp
-type PitchClass = PCLetter * Accidental
-// type RomanNumeral =
-
-
-type Quality = Major | Minor
 type Scale = int Set // uint?
-type Key = PitchClass * Quality
 
 let chromatic : Scale = Set.ofList [1..12] // root omitted; last element is the period/modulus of the scale
 let ionian : Scale = Set.ofList [2;4;5;7;9;11;12]
+let pent : Scale = Set.ofList [2;4;7;9;12]
 
-let Period (s : Scale) = s.MaximumElement
+let Period (s : Scale) : int = s.MaximumElement
 
-let ReSeat x offset modulus =
+let BinEnc (s : Scale) : int = // a unique, *dense* binary encoding for every scale of any cardinality, as an integer
+    [ for n in s -> 2. ** float (n - 1) ] |> List.sum |> int
+
+let ReSeat x offset modulus : int = // determines the relationship of `offset` to `x` mod `modulus`
     let diff = x - offset
     match diff > 0 with
         | true -> diff
         | false -> diff + modulus
 
-let (>>>) (s : Scale) (offset : int) =
+let (>>>) (s : Scale) (offset : int) : Scale = // modulates a scale; returns the nth mode of a scale; inverts chords
     let index = (offset % s.Count)
 
     if index = 0 then s
@@ -40,20 +41,52 @@ let (>>>) (s : Scale) (offset : int) =
 
 let (<<<) (s : Scale) (offset : int) = s >>> s.Count - offset
 
-let BinEnc (s : Scale) =
-    [ for n in s -> 2. ** float (n - 1) ] |> List.sum |> int
+let Modes(s : Scale) : List<Scale> = // returns all the modes of a scale; all the inversions of a chord.
+    [ for i in 0..(s.Count-1) -> s >>> i ]
+
+let PrimeMode (s : Scale) : Scale = // smallest binary-encoded/most "left-compact" mode; root inversion of a chord
+    let x = [ for i in Modes s -> (BinEnc i, i) ]
+    (List.sort x)[0] |> snd
 
 //let ForteNo (s : Scale) =
 
-let Modes(s : Scale) =
-    [ for i in 0..(s.Count-1) -> s >>> i ]
-
-let PrimeMode (s : Scale) =
-    let m = Modes s
-    let x = [ for i in m -> (BinEnc i, i) ]
-    (List.sort x)[0] |> snd
-
-//type Chord =
-
 // let isEnharmonic =
 // TO-DO
+
+(***
+        CHORDS
+***)
+
+type Chord = Scale // it’s that easy
+let major : Chord = Set.ofList [4;7;12]
+let minor : Chord = Set.ofList [3;7;12]
+
+type Degree = int // ordinal,
+//type RomanNumeral =
+
+type Letterform = string // best way to do this?
+type Accidental = int * string
+type Alphabet = Letterform List * Accidental List * Scale
+type PitchClass = Letterform * Accidental
+
+let DblFlat : Accidental = (-2, "𝄫")
+let Flat : Accidental = (-1, "♭")
+let Natural : Accidental = (0, "♮")
+let Sharp : Accidental = (1, "♯")
+let DblSharp : Accidental = (2, "𝄪")
+
+let anglo : Alphabet = ([for c in 'A'..'G' -> string c], [DblFlat;Flat;Natural;Sharp;DblSharp], ionian >>> 5)
+let german : Alphabet = (["A";"B";"H";"C";"D";"E";"F";"G"], [DblFlat;Flat;Natural;Sharp;DblSharp], (ionian >>> 5).Add(4))
+let solfege : Alphabet = (["do";"re";"mi";"fa";"sol";"la";"ti"], [DblFlat;Flat;Natural;Sharp;DblSharp], ionian)
+let hindustani : Alphabet = (["सा";"रे";"ग";"म";"प";"ध";"नि"], [], ionian)
+
+type Quality = Major | Minor
+
+type Key = PitchClass * Quality
+
+let KeyMapping (n : PitchClass) (s : Scale) (a : Alphabet) =
+    // cycle both `a`’s Letterform List and `a`’s Scale until the first element of the former matches `n.fst`
+    // if `n` has an accidental, set it (sharp/flat)
+    // compare `s` to `a`’s scale, use difference to decide further accidentals (i.e. in D♭ major, D♭ ⇢ E♭ ⇢ F♮ because (anglo.scale >>> 4)[2] = 3 <   major[2] = 4, and Natural is 1 greater than Flat)
+    //maybe
+    n
